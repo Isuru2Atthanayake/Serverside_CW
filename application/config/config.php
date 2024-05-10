1b52e39291ed8ecd95366d6275d8dcee7be430a1
Isuru Atthanayake <79752561+Isuru2Atthanayake@users.noreply.github.com>
2024-05-10 12:35:16 +0530

    second commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = '';
-$config['base_url'] = 'http://localhost/Tutorials/cw/tute5/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -226,8 +225,8 @@ $config['allow_get_array'] = TRUE;
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 0;
-
+// $config['log_threshold'] = 0;
+$config['log_threshold'] = 2;
 /*
 |--------------------------------------------------------------------------
 | Error Logging Directory Path
@@ -384,14 +383,14 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_samesite'] = 'Lax';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = 'to_dolist.ci_sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
-$config['sess_regenerate_destroy'] = FALSE;
+$config['sess_regenerate_destroy'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
